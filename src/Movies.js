521f719d3100d9eaf5716fd32227ba6e5533eaaf
Lia Ximenes <ximeneslia@gmail.com>
2021-05-11 22:36:18 -0300

    tentando dar run build

--- src/Movies.js
+++ src/Movies.js
@@ -1,4 +1,17 @@
+import { useState, useEffect } from "react"
+import axios from 'axios';
+
 export default function Movies(){
+   // const [movie, setMovie] = useState([]);
+
+    useEffect(() => {
+        const request = axios.get("https://mock-api.bootcamp.respondeai.com.br/api/v2/cineflex/movies");
+    promisse.then((answer) => console.log(answer.data))
+    }, []);
+
+    //answer => {setMovie(resposta.data)}
+
+
     return(
         <div class="content">
             <div class="title"><p>Selecione um filme</p></div>
